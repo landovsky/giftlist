5c8051222e9e7dde731ce3ac03765eddb4ebd70f
landovsky <landovsky@gmail.com>
2017-03-22 11:06:24 +0100

    fix

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,9 +1,10 @@
 class StatsController < ApplicationController
   def index
-  if session[:user_id] = 1
-    @users = User.all
-    @lists = List.all
-  else
-     redirect_to home_path
+    if session[:user_id] = 1
+      @users = User.all
+      @lists = List.all
+    else
+      redirect_to home_path
+    end
   end
 end
