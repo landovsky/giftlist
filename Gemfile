17b9852b26ff706d8578bb6c0b80d4e63a4c7663
landovsky <landovsky@gmail.com>
2017-03-22 06:13:13 +0100

    seeds

--- Gemfile
+++ Gemfile
@@ -53,7 +53,7 @@ group :development do
   gem 'capistrano-rails', '~> 1.2'
   gem 'capistrano-passenger', '~> 0.2.0'
   gem 'capistrano-rbenv', '~> 2.1'
-  gem 'passenger'
+  gem 'seed_dump'
  end
 
 gem 'net-ssh', '>=3.0.0'
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Gift.create!([
+  {list_id: 27, user_id: nil, name: "zabraný dárek", description: "20 kusů krásných štíhlých kořenek", price_range: "max 350 Kč"},
+  {list_id: 27, user_id: nil, name: "něco sladkého", description: "třeba z cukrárny", price_range: "350..450 Kč"}
+])
+InvitationList.create!([
+  {user_id: 3, list_id: 4},
+  {user_id: 1, list_id: 13},
+  {user_id: 1, list_id: 17},
+  {user_id: 1, list_id: 18}
+])
+List.create!([
+  {user_id: 1, occasion: "svatba priklad", occasion_of: "Tom & Dita"}
+])
+User.create!([
+  {email: "landovsky@gmail.com", role: "registered", name: "Tomáš", surname: "Landovský", sex: nil, dob: nil, birth_year: nil, password_digest: "$2a$10$ruQct.vUHXg562ZLn7Kvce2bdqfh7sEVOQX4ZGjJ3PIsfgF1opQIS"}
+])
