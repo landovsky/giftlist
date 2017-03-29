ba10bd57eda76ae230f500131cdf694c4dfff268
landovsky <landovsky@gmail.com>
2017-03-29 22:25:17 +0200

    tests

--- test/controllers/url_controller_test.rb
+++ test/controllers/url_controller_test.rb
@@ -1,14 +1,2 @@
 require "test_helper"
 
-describe UrlController do
-  it "should get create" do
-    get url_create_url
-    value(response).must_be :success?
-  end
-
-  it "should get destroy" do
-    get url_destroy_url
-    value(response).must_be :success?
-  end
-
-end
--- test/models/url_test.rb
+++ test/models/url_test.rb
@@ -1,9 +1,2 @@
 require "test_helper"
 
-describe Url do
-  let(:url) { Url.new }
-
-  it "must be valid" do
-    value(url).must_be :valid?
-  end
-end
