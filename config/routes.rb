8045d7b417c9733d33a2009ad663b18f819bdb33
landovsky <landovsky@gmail.com>
2017-04-05 18:35:48 +0200

    tweaks

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'home#index'
+  root 'lists#index'
 
   post 'urls' => 'urls#create', as: 'urls'
   get 'urls/destroy/:id' => 'urls#destroy', as: 'url_destroy'
