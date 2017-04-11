37b05f02baf8fa991cf2fbd32a0d8151c47d1fca
landovsky <landovsky@gmail.com>
2017-04-11 16:01:56 +0200

    token fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,8 +28,13 @@ class User < ApplicationRecord
     end
   end
   
-  def token_for_list(list_id)
-    JsonWebToken.encode(user_id: self.id, list_id: list_id, exp: 6.months.from_now)
+  def token_for_list(options={})
+    list_id = options[:list_id] if options[:list_id]
+    interval = options[:interval] if options[:interval]
+    interval ||= "months"
+    n = options[:n] if options[:n]
+    n ||= 6
+    JsonWebToken.encode(user_id: self.id, list_id: list_id, exp: n.send(interval).from_now.to_i)
   end
 
   #TODO pomocná metoda
--- lib/test.rb
+++ lib/test.rb
@@ -4,7 +4,13 @@ def tim(n,interval)
 end
 
 def cycle(n,interval)
-  t = JsonWebToken.encode(:id => 5, :exp => tim(n,interval))
-  out = JsonWebToken.decode(t)
-  out = DateTime.strptime(out["exp"].to_s,'%s')
+  u = User.load_random
+  t = u.token_for_list(list_id: 5, interval: interval, n: n)
+  #t = JsonWebToken.encode(:id => 5, :exp => tim(n,interval))
+  decode_helper(t)
+end
+
+def decode_helper(token)
+  d = JsonWebToken.decode(token)
+  out = DateTime.strptime(d["exp"].to_s,'%s')
 end
