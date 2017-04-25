0c6f10bf3452a75149086b60b8f0a7c89733dac8
landovsky <landovsky@gmail.com>
2017-04-25 16:23:14 +0200

    fix for delayed job plugin

--- config/initializers/delayed_job.rb
+++ config/initializers/delayed_job.rb
@@ -1,5 +1,6 @@
 # config/initializers/delayed_job.rb
-require 'delayed_duplicate_prevention_plugin'
+
+#require 'delayed_duplicate_prevention_plugin'
 
 Delayed::Backend::ActiveRecord::Job.send(:include, DelayedDuplicatePreventionPlugin::SignatureConcern)
 Delayed::Worker.plugins << DelayedDuplicatePreventionPlugin
