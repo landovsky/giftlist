27e4b1f89ba19d25562337bef5111794f5795bd1
landovsky <landovsky@gmail.com>
2017-04-26 09:23:16 +0200

    status mailer

--- test/mailers/previews/status_mailer_preview.rb
+++ test/mailers/previews/status_mailer_preview.rb
@@ -1,8 +1,8 @@
 # Preview all emails at http://localhost:3000/rails/mailers/status_mailer
 class StatusMailerPreview < ActionMailer::Preview
 
-def delayed_jobs(jobs_count: 5)
-  StatusMailer.delayed_jobs
+def delayed_jobs
+  StatusMailer.delayed_jobs(jobs_count: 5)
 end
 
 end
