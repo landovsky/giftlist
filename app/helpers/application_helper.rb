2fee31c62f02c81a56c474d3fded77771df49695
landovsky <landovsky@gmail.com>
2017-05-10 11:43:13 +0200

    analytics_tracker

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -17,4 +17,13 @@ module ApplicationHelper
     ["stage", "test", "development"].include?(Rails.env) ? Rails.env : ""
   end
   
+  def analytics_tracker
+    case Rails.env
+    when "stage"
+      "UA-349441-6"
+    when "production"
+      "UA-349441-5"
+    end
+  end
+  
 end
