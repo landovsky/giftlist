9587e2ae28ddf687597bafd8e22df74e0f9c6ac6
landovsky <landovsky@gmail.com>
2017-05-12 22:03:54 +0200

    token fix

--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -2,13 +2,13 @@ class JsonWebToken
   class << self
     def encode(payload, exp = 24.hours.from_now)
       payload[:exp] ||= exp.to_i
-      key = ENV["SECRET_KEY_BASE"]
-      Rails.logger.warn "#{self}: #{__method__}: JWT debug: key: #{key}"
+      #key = ENV["SECRET_KEY_BASE"]
+      key = Rails.application.secrets.secret_key_base
       JWT.encode(payload, key)
     end
 
     def decode(token)
-      body = JWT.decode(token, ENV["SECRET_KEY_BASE"])[0]
+      body = JWT.decode(token, Rails.application.secrets.secret_key_base)[0]
       HashWithIndifferentAccess.new body
     rescue
       nil
