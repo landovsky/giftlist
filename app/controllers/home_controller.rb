a6d64b1237214657f1587e95420a1d3c72fc1b6b
landovsky <landovsky@gmail.com>
2017-06-06 21:05:49 +0200

    title_page tweaks

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,11 +3,11 @@ class HomeController < ApplicationController
 
   def index
     @data = [
-      ['pushpin','Když chceš mít jedno místo kam si během roku zapíšeš co by tě potěšilo.','vytvoř si seznamy pro různé příležitosti'],
+      ['send','Když chceš mít pro rodinu a známé odpověď na otázku "a co by si chtěl k narozeninám?"','nasdílej s nimi svůj seznam'],
+      ['check','Když nechceš dostávat stejné dárky dvakrát.','nech známé rezervovat dárky'],
+      ['file','Když chceš mít jedno místo, kam si během roku zapíšeš co by tě potěšilo.','vytvoř si seznamy pro různé příležitosti'],
       ['barcode','Když už nechceš k vánocům a narozeninám dostávat cetky.','přej si konkrétní věci'],
-      ['link','Když máš jasno v tom co chceš.','přidej odkazy do e-shopů'],
-      ['send','Když chceš mít pro rodinu a známé odpověď na otázku "a co bych si chtěl k narozeninám?"','nasdílej s nimi svůj seznam'],
-      ['check','Když nechceš dostat stejné dárky dvakrát.','nech známé rezervovat dárky']
+      ['link','Když máš jasno v tom co si přeješ.','přidej odkazy do e-shopů']
     ]
   end
 
