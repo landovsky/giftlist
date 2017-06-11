fec6f17b8f2fb3c9750e055169bf881f1da92d6b
landovsky <landovsky@gmail.com>
2017-06-11 22:35:54 +0200

    rollbar

--- lib/my_logger.rb
+++ lib/my_logger.rb
@@ -3,5 +3,6 @@ module MyLogger
     options[:level] ? level = options[:level] : level = "debug"
     content = "#{Time.new.localtime} MYLOGGER (#{level}), #{topic}: #{msg}: #{options.except(:level)} | #{caller[0]} / #{caller[1]}"
     Rails.logger.send(level, content)
+    Rollbar.log(level, content)
   end
 end
