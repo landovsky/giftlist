2211c0b5ccdc01a3e9ee9803298ee566ce32fd3c
landovsky <landovsky@gmail.com>
2017-12-01 23:35:16 +0100

    add missing bootstrap.min

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,7 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require bootstrap
+//= require bootstrap.min
 //= require bootstrap-sprockets
 //= require moment
 //= require bootstrap-datetimepicker
