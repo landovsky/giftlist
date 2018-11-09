02adf69e040068b983b52f3234bf19d403de62c4
Tomas Landovsky <435847+landovsky@users.noreply.github.com>
2018-11-09 22:00:53 +0100

    Sendgrid smtp

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,17 +1,15 @@
+ActionMailer::Base.smtp_settings = {
+  user_name: ENV['SENDGRID_USERNAME'],
+  password: ENV['SENDGRID_PASSWORD'],
+  domain: 'www.givit.cz',
+  address: 'smtp.sendgrid.net',
+  port: 587,
+  authentication: :plain,
+  enable_starttls_auto: true
+}
+
 Rails.application.configure do
-
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-  address:              'smtp.gmail.com',
-  port:                 587,
-  domain:               'gmail.com',
-  user_name:            'givit.cz@gmail.com',
-  password:             'giftlistPass01',
-  authentication:       :login,
-  enable_starttls_auto: true  }
-
-
-
+  config.action_mailer.delivery_method = :smtp 
   # Settings specified here will take precedence over those in config/application.rb.
 
   Rails.application.routes.default_url_options[:host] = 'www.givit.cz'
